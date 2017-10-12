2e690ffc42a0e959bd6b8842bf2561b581793934
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2017-10-12 16:47:27 -0700

    First commit

---
+++ core/src/com/mygdx/game/MeteorScreen.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Created by manla on 10/12/2017.
+ */
+
+public class MeteorScreen extends ScreenAdapter
+{
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+
+
+
+public class MyGdxGame extends Game
+{
+    @Override
+    public void create() {
+        setScreen(new MeteorScreen());
+    }
+}
